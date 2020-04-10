18bbfbec8192d6d5086bdf2ac9d4c15f123472d2
alvin <alvinhtml@gmail.com>
2020-04-10 20:19:45 +0800

    user list sort

--- app/components/User.jsx
+++ app/components/User.jsx
@@ -7,18 +7,21 @@ import {Button} from 'react-miniui';
 
 
 // 定义自己的 Hooks
-const useUser = (page: number) => {
+const useUser = () => {
   const [loading, setLoading] = useState(false);
   const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [order, setOrder] = useState(['name', 'asc']);
 
   const loadUsers = async () => {
+
     try {
       const users = await superagent.get('/api/user')
-        .send({
+        .query({
           page,
+          order: `${order[0]},${order[1]}`
         });
 
-      console.log('users.body', users, users.body);
       setUsers(users.body);
       setLoading(false);
     } catch(err) {
@@ -29,33 +32,35 @@ const useUser = (page: number) => {
   useEffect(() => {
     setLoading(true);
     loadUsers();
-  }, [page]);
+  }, [page, order]);
 
-  return [loading, users];
+
+  return [loading, users, (page, order) => {
+    setPage(page);
+    setOrder(order);
+  }];
 };
 
 
+const rowKey = 'id';
 
 function User() {
-  const [loading, users] = useUser(1);
-
-  if (loading) {
-    return <div>正在加载。。。</div>
-  }
-
-
+  const [loading, users, reload] = useUser();
   console.log(users);
 
   return (
     <div>
       <List
-        users={users}
-        reLoad={() => {
-
+        rows={users}
+        rowKey={rowKey}
+        reload={({page, order}) => {
+          console.log(page, order);
+          reload(page, order);
         }}
       />
     </div>
   );
 }
+
 
 export default User;
--- app/components/user/List.jsx
+++ app/components/user/List.jsx
@@ -1,27 +1,12 @@
 import * as React from 'react';
-import {Button} from 'react-miniui';
+import {ButtonGroup, Button, Input, FormGroup} from 'react-miniui';
 
 import Lister, {Column, withLister} from '~/../../react-lister/dist/index.js';
 import'~/../../react-lister/dist/lister.css';
 import UserActions from '~/components/user/UserActions';
 
-
-const options = {
-  order: true, // 是否需要排序
-  visibility: true, // 是否可见
-  width: 200, // 列表的初始宽度，默认 200
-  resize: true // 是否允许拖动改变列宽度
-}
-
-const columns = [
-  new Column('用户', Column.getValue('name'), row => <span>{row.name}</span>, options),
-  new Column('邮箱', Column.getValue('email'), row => <span>{row.name}</span>, options),
-  new Column('角色', Column.getValue('type'), row => <span>{row.name}</span>, options),
-  new Column('操作', null, row => <UserActions row={row} />, {resize: false})
-]
-
 class UserList extends React.Component {
-  lister = React.createRef();
+  liste = React.createRef();
 
   constructor(props) {
     super(props);
@@ -31,7 +16,8 @@ class UserList extends React.Component {
   }
 
   toggleSelectAll() {
-    this.lister.current.toggleSelectAll();
+    console.log("this.liste", this.liste);
+    this.liste.current.toggleSelectAll();
   }
 
   handleSelect = (selectedIDs: Array<string>) => {
@@ -42,30 +28,47 @@ class UserList extends React.Component {
 
   render() {
 
-    const {selectAll, users} = this.props;
+    const {toggleSelectAll, rows, reload, createRef, columns} = this.props;
 
     const {selectedIDs} = this.state;
 
-    console.log("selectedIDs", selectedIDs);
+    console.log("columns-", columns);
 
     return (
       <div>
         <Lister
-          ref={this.lister}
-          rows={users}
+          ref={createRef}
+          rows={rows}
           total={103}
           columns={columns}
           page={1}
           selectable={true}
           onSelect={this.handleSelect}
+          reload={reload}
         >
-          <Button type="basic" onClick={this.toggleSelectAll.bind(this)}>选择全部</Button>
+          <ButtonGroup>
+            <Button color="blue" onClick={toggleSelectAll}>全选</Button>
+          </ButtonGroup>
+          <FormGroup>
+            <Input type="text" />
+            <Button color="teal">搜索</Button>
+          </FormGroup>
         </Lister>
       </div>
     );
   }
 }
 
-console.log("withLister--", withLister);
+const options = {
+  order: true, // 是否需要排序
+  visibility: true, // 是否可见
+  width: 200, // 列表的初始宽度，默认 200
+  resize: true // 是否允许拖动改变列宽度
+}
 
-export default withLister(UserList);
+export default withLister(UserList, 'user', [
+  new Column('用户', 'name', row => <span>{row.name}</span>, options),
+  new Column('邮箱', 'email', row => <span>{row.email}</span>, options),
+  new Column('角色', 'type', row => <span>{row.type}</span>, options),
+  new Column('操作', null, row => <UserActions row={row} />, {resize: false})
+]);
